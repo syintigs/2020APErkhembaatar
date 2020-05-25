f85e6c1f08a51348d3152cd39410947b1bf38ebd
Juan Carlos <zolmonjuancarlos@gmail.com>
2020-05-25 21:34:50 +0800

    test done

--- Englishtest/Source/Englishtest/Englishtest.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "Englishtest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Englishtest, "Englishtest" );
